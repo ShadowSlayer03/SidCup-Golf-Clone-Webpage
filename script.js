422cc780413d914dc7fc19162f2a0eac7cb6fdd8
Arjun R Nambiar <95574870+ShadowSlayer03@users.noreply.github.com>
2023-11-20 22:52:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,311 +1,164 @@
-////////////////////////////////////
-// Normal Button and other Logic //
-///////////////////////////////////
+let cursor = document.querySelector("#cursor");
+let cursorBlur = document.querySelector("#cursor-blur");
+let options = document.querySelectorAll("#nav h4");
+let elementsHeading = document.querySelectorAll(".elem h2");
+let testimonialPara = document.querySelector("#testimonial p");
+let p4Heading = document.querySelector("#page4 h1");
+let arrowDivContainer = document.querySelector("#arrow-div-container");
 
-const moveUpBtn = document.querySelector("#move-up");
-const moveDownBtn = document.querySelector("#move-down");
-const vehiclePreview = document.querySelector("#vehicle-preview");
-//const smokeVideo = document.querySelector(".smoke");
-const vehicleIcons = document.querySelectorAll(".vehicle-icon");
-let dropdownToggleBtns = document.querySelectorAll(".dropdown-toggle");
-let dropDownValues = document.querySelectorAll(".dropdown-menu ul li");
-let selectedPrice = document.querySelector("#selected-price");
-let selectedYear = document.querySelector("#selected-year");
-let vehicleYear = document.querySelector("#vehicle-year");
-let vehiclePrice = document.querySelector("#vehicle-price");
+let comments = [
+    "Great facilities — loads of bays in the driving range with many coloured targets to aim for and a ball tracking monitor to see if you’re getting close.",
+    "I absolutely love the adventure golf here. It seems every time I come they’ve added a new feature. My mum and I usually come together and it’s such a laugh. The designs for the holes are creative and the two different courses makes it more fun as you can complete both and compare your scores!",
+    "Absolutely loved the experience! The staff looked after me ensured I was enjoying the range and gave me helpful tips to get the best out of my game. Digital screens to see my progress. I’ll be back 😁 🏌🏽‍♀️",
+    "Excellent couple of hours, relax and enjoy in the fun. Staff were accommodating, friendly and very helpful. Café on site for refreshments etc. Will keep children enterntained during the holidays. <br>Worth a visit if you haven’t been!"
+]
 
-let blueBanner = document.querySelector("#blue-banner")
-let page1 = document.querySelector("#page1")
-let navBar = document.querySelector("nav");
-let cursor = document.querySelector(".cursor");
-
-let vehicle3Icon = document.querySelector("#vehicle3");
-let vehicle3Img = document.querySelector("#vehicle3 img");
-
-
-let currentIndex = 3;
-let startingPhotoIndex = 1;
-let endingPhotoIndex = 5;
-
-
-//Mouse follower
-document.body.addEventListener('mousemove', (event) => {
- cursor.style.transform = `translate(${event.clientX-10}px, ${event.clientY-10}px)`;
-});
-
-moveUpBtn.addEventListener('mouseenter',()=>{
-    cursor.classList.add("cursor-on-buttons");
-})
-moveDownBtn.addEventListener('mouseenter',()=>{
-    cursor.classList.add("cursor-on-buttons");
-})
-moveUpBtn.addEventListener('mouseleave',()=>{
-    cursor.classList.remove("cursor-on-buttons");
-})
-moveDownBtn.addEventListener('mouseleave',()=>{
-    cursor.classList.remove("cursor-on-buttons");
+document.addEventListener("mousemove",(event)=>{
+    cursor.style.left = event.x + "px";
+    cursor.style.top = event.y+ "px";
 })
 
+document.addEventListener("mousemove",(event)=>{
+    cursorBlur.style.left = event.x -200 + "px";
+    cursorBlur.style.top = event.y -200 + "px";
+})
 
-//Up button animations
-let timesCalledUp=0;
-let jUp;
+let i=0;
+setInterval(()=>{
+    if(i===3){ i=0; }
+    testimonialPara.textContent = comments[i];
+    i++;
+},5000)
 
-function addPaletteUpAnimation() {
-    let arrUp = [1,2,3,4,5];
-    let k = timesCalledUp;
-    let timesThisExecutes=0;
-
-    console.log("Iteration "+timesCalledUp);
-
-    for (let i = 0; i < vehicleIcons.length; i++) {
-        if (i == 0 && timesCalledUp==0)
-            jUp = timesCalledUp;
-
-        if(timesCalledUp>=1 && i==0)
-            jUp = 5-timesCalledUp;
-
-        // if(timesCalledUp>=1 && i==1)
-        //     jUp = timesCalledUp-1;
-        console.log(`Applying v${arrUp[jUp]}Up animation to element at index ${i}`);
-
-        if (arrUp[jUp] == 4) {
-            vehicleIcons[i].children[0].style.animation = `enlargeSize 1s ease-in-out forwards`;
-        } else {
-            vehicleIcons[i].children[0].style.animation = 'none';
-        }
-
-        if(k!=0)
-        {
-            vehicleIcons[i].style.animation = `v${arrUp[jUp]}Up 1s ease-in-out forwards`;
-            jUp++;k--;
-        }
-
-        else if(k==0)
-        {
-            if(timesThisExecutes==0)
-            { jUp=0; }
-
-            vehicleIcons[i].style.animation = `v${arrUp[jUp]}Up 1s ease-in-out forwards`;
-            jUp++;
-            timesThisExecutes++;
-        }
-
+gsap.to("#nav",{
+    backgroundColor: "#000",
+    height: "110px",
+    duration: 0.5,
+    scrollTrigger: {
+        trigger: "#nav",
+        scroller: "body",
+        //markers: true,
+        start: "top -10%",
+        end: "top -11%",
+        scrub: 1
     }
-    if(timesCalledUp==5)
-    timesCalledUp = 0;
-    else
-    timesCalledUp++;
-    timesCalledDown=5-timesCalledUp;
-}
-
-let isAnimationInProgress = false;
-
-moveUpBtn.addEventListener('click',()=>{
-
-    if (isAnimationInProgress) {
-        return;
-    }
-
-    isAnimationInProgress = true;
-
-    const timeline1 = gsap.timeline({
-        onComplete: ()=>{
-            isAnimationInProgress = false;
-        }
-    });
-
-    timeline1.to("#vehicle-preview",{
-        x: "60%",
-        duration: 1.5,
-        opacity: 0,
-    })
-
-    if(currentIndex==5)
-    currentIndex = 1;
-    else
-    currentIndex++;
-
-    setTimeout(()=>{
-        vehicle3Icon.style.backgroundColor = "#dae9fa";
-        vehicle3Icon.style.height = "70px"
-        vehicle3Icon.style.width = "70px"
-        vehicle3Img.style.height = "30px";
-    },800)
-
-    setTimeout(addPaletteUpAnimation,500);
-
-
-    setTimeout(()=>{
-        vehiclePreview.setAttribute('src',`./images/image${currentIndex}.png`);
-        timeline1.to("#vehicle-preview",{
-            x: "-70%",
-            opacity: 0,
-            duration: 0.5,
-        })
-        setTimeout(()=>{
-            timeline1.to("#vehicle-preview",{
-                x: "0%",
-                duration: 1.5,
-                opacity: 1,
-            })
-        },650)
-     },1700)
-
-        moveDownBtn.disabled = false;
-        moveUpBtn.disabled = false;
-    
-})
-// *************** Previous Bike Preview change Logic ************
-// vehiclePreview.style.transform = "none";
-// vehiclePreview.style.transform = "translateX(70%)";
-// vehiclePreview.style.opacity = "0";
-// if(currentIndex==5)
-// currentIndex = 1;
-// else
-// currentIndex++;
-// setTimeout(() => {
-//     vehiclePreview.setAttribute('src',`./images/image${currentIndex}.png`);
-//     vehiclePreview.style.left = "-30%";
-//     //vehiclePreview.style.transform = "translateX(500px)";
-//     vehiclePreview.style.opacity = 0.5;
-//     setTimeout(()=>{
-//         vehiclePreview.style.opacity = 1;
-//         vehiclePreview.style.left = "0%";
-//     },100)
-// }, 700);
-// **********************************************************
-
-
-//****** Previous Change Palette Down****************
-//  function changeIconsDown()
-//  {
-//     endingPhotoIndex--;
-
-//      if(endingPhotoIndex==1)
-//      endingPhotoIndex=5;
-
-//      let j = endingPhotoIndex;
-//      for(let i=0;i<5;i++)
-//      {
-//          if(j==0)
-//          j=5;
-//          console.log("Before decrement",j);
-//          vehicleIcons[i].innerHTML = `<img src="./images/image${j}.png" alt="vehicles">`
-//          j--;
-//          console.log("After decrement",j);
-//      }
-//  }
-//**********************************************************
-
-//Down button animations
-let timesCalledDown=0;
-let jDown;
-function addPaletteDownAnimation()
-{
-    let arr = [1,2,3,4,5];
-    for (let i = 0; i < vehicleIcons.length; i++) {
-        if(i==0)
-        jDown=timesCalledDown;
-        if(jDown==5)
-        jDown=0;
-        console.log(`Applying v${arr[jDown]}Down animation to element at index ${i}`);
-        if(arr[jDown]==2)
-            vehicleIcons[i].children[0].style.animation = `enlargeSize 1s ease-in-out forwards`;
-        else 
-            vehicleIcons[i].children[0].style.animation = `none`;
-        vehicleIcons[i].style.animation = `v${arr[jDown]}Down 1s ease-in-out forwards`;
-        jDown++;
-    }
-    if(timesCalledDown==5)
-    timesCalledDown = 0;
-    else
-    timesCalledDown++;
-    timesCalledUp=5-timesCalledDown;
-}
-
-moveDownBtn.addEventListener('click',()=>{
-
-    const timeline2 = gsap.timeline();
-
-    setTimeout(()=>{
-        vehicle3Icon.style.backgroundColor = "#dae9fa";
-        vehicle3Icon.style.height = "70px"
-        vehicle3Icon.style.width = "70px"
-        vehicle3Img.style.height = "30px";
-    },800)
-
-    setTimeout(addPaletteDownAnimation,400);
-    
-    timeline2.to("#vehicle-preview",{
-        x: "-60%",
-        duration: 1.5,
-        opacity: 0,
-    })
-
-    if(currentIndex==1)
-    currentIndex = 5;
-    else
-    currentIndex--;
-
-
-    setTimeout(()=>{
-        vehiclePreview.setAttribute('src',`./images/image${currentIndex}.png`);
-        timeline2.to("#vehicle-preview",{
-            x: "70%",
-            opacity: 0,
-            duration: 0.1,
-        })
-        setTimeout(()=>{
-            timeline2.to("#vehicle-preview",{
-                x: "0%",
-                duration: 1.5,
-                opacity: 1,
-            })
-        },650)
-     },1700)
-
-    moveDownBtn.disabled = false;
-    moveUpBtn.disabled = false;
 })
 
-//Toggle Dropdowns
- dropdownToggleBtns.forEach(function (toggle) {
-         toggle.addEventListener("click", function () {
-             if(this.parentNode.querySelector(".dropdown-menu").style.display === "block")
-             this.parentNode.querySelector(".dropdown-menu").style.display = "none";
-            else
-            this.parentNode.querySelector(".dropdown-menu").style.display = "block";
-             event.stopPropagation(); // Prevent the click event from reaching the window
-         });
+gsap.to("#main",{
+    backgroundColor: "#000",
+    scrollTrigger: {
+        trigger: "#main",
+        scroller: "body",
+        // markers: true,
+        start: "top -25%",
+        end: "top -70%",
+        scrub: 2
+    }
+})
 
-      //Close dropdown menu if user clicks outside of it
-      window.addEventListener("click", function (event) {
-          if (!event.target.classList.contains("dropdown-toggle")) {
-              var dropdowns = document.getElementsByClassName("dropdown-menu");
-              for (var i = 0; i < dropdowns.length; i++) {
-                  var openDropdown = dropdowns[i];
-                  if (openDropdown.style.display==="block") {
-                      openDropdown.style.display="none";
-                  }
-             }
-         }
-      });
- });
-
-
- //Value selected in dropdown occurs 
- dropDownValues.forEach((ddVal)=>{
-        ddVal.addEventListener('click',(event)=>{
-            if(vehicleYear.contains(event.target))
-            {
-                selectedYear.innerHTML = event.target.textContent;
-            }
-            if(vehiclePrice.contains(event.target))
-            {
-                selectedPrice.innerHTML = event.target.textContent;
-            }
+options.forEach(function(elem){
+    elem.addEventListener('mouseenter',()=>{
+        cursor.style.scale = 3;
+        cursor.style.border = "0.5px solid white";
+        cursor.style.backgroundColor = "transparent";
     })
- })
- 
+    elem.addEventListener('mouseleave',()=>{
+            cursor.style.scale = 1;
+            cursor.style.border = "none";
+            cursor.style.backgroundColor = "#95c11e"
+    })
+})
 
+elementsHeading.forEach(function(elem){
+    elem.addEventListener('mouseenter',()=>{
+            console.log(p4Heading);
+            p4Heading.classList.add("green-outline");
+            cursor.style.scale = 3;
+            cursor.style.border = "0.5px solid white";
+            cursor.style.backgroundColor = "transparent";
+    })
+    elem.addEventListener('mouseleave',()=>{
+            console.log(p4Heading);
+            p4Heading.classList.remove("green-outline");
+            cursor.style.scale = 1;
+            cursor.style.border = "none";
+            cursor.style.backgroundColor = "#95c11e"
+    })
+})
+
+gsap.from("#about-us img, #about-us-info",{
+    y: 90,
+    opacity: 0,
+    duration: 2,
+    stagger: 0.6,
+    scrollTrigger: {
+        trigger: "#about-us",
+        scroller: "body",
+        //markers: true,
+        start: "top 60%",
+        end: "top 55%",
+        scrub: 2
+    }
+})
+
+gsap.from("#cards-container",{
+    opacity: 0,
+    duration: 2,
+    stagger: 0.6,
+    scrollTrigger: {
+        trigger: "#cards-container",
+        scroller: "body",
+        //markers: true,
+        start: "top 65%",
+        end: "top 60%",
+        scrub: 2
+    }
+})
+
+gsap.from("#colon1",{
+    y: -70,
+    x: -70,
+    scrollTrigger: {
+        trigger: "#colon1",
+        scroller: "body",
+        // markers: true,
+        start: "top 50%",
+        end: "top 46%",
+        scrub: 5
+    }
+})
+
+gsap.from("#colon2", {
+    x: "100%",  // Start at 100% right
+    y: "100%",  // Start at 100% bottom
+    scrollTrigger: {
+        trigger: "#colon2",
+        scroller: "body",
+        start: "top 90%",  // Adjust the start point
+        end: "top 50%",   // Adjust the end point
+        scrub: 5  // Adjust scrub value
+    }
+})
+
+gsap.from("#page4 h1",{
+    y: 30,
+    scrollTrigger: {
+        trigger: "#page4 h1",
+        scroller: "body",
+        start: "top 75%",  // Adjust the start point
+        end: "top 70%",   // Adjust the end point
+        scrub: 3  // Adjust scrub value
+    }
+})
+
+arrowDivContainer.addEventListener('mouseenter',()=>{
+    cursor.style.scale = 4;
+    cursor.style.border = "0.5px solid white";
+    cursor.style.backgroundColor = "transparent";
+})
+
+arrowDivContainer.addEventListener('mouseleave',()=>{
+    cursor.style.scale = 1;
+    cursor.style.border = "none";
+    cursor.style.backgroundColor = "#95c11e"
+})
